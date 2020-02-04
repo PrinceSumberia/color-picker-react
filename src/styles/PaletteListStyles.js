5c4b0a5832943a529b178804b99cf8d7425f5ced
Prince Sumberia <broughtmeup@gmail.com>
2020-02-04 15:11:01 +0530

    Make PaletteList Responsive

--- src/styles/PaletteListStyles.js
+++ src/styles/PaletteListStyles.js
@@ -1,3 +1,5 @@
+import sizes from './sizes';
+
 export default {
 	root: {
 		backgroundColor: 'blue',
@@ -12,7 +14,16 @@ export default {
 		display: 'flex',
 		alignItems: 'flex-start',
 		flexDirection: 'column',
-		flexWarp: 'wrap'
+		flexWarp: 'wrap',
+		[sizes.down('xl')]: {
+			width: '75%'
+		},
+		[sizes.down('md')]: {
+			width: '68%'
+		},
+		[sizes.down('xs')]: {
+			width: '65%'
+		}
 	},
 	nav: {
 		display: 'flex',
@@ -29,6 +40,13 @@ export default {
 		width: '100%',
 		display: 'grid',
 		gridTemplateColumns: 'repeat(3, 30%)',
-		gridGap: '5%'
+		gridGap: '2rem',
+		[sizes.down('md')]: {
+			gridTemplateColumns: 'repeat(2, 50%)'
+		},
+		[sizes.down('xs')]: {
+			gridTemplateColumns: 'repeat(1, 100%)',
+			gridGap: '1rem'
+		}
 	}
 };
